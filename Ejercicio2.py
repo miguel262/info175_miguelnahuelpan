f95856a2cee4feda7ace83743515910343d23d52
miguel262 <miguel.nahuelpan@alumnos.uach.cl>
2016-03-17 14:55:13 -0300

    ejercicio2

---
+++ Ejercicio2.py
@@ -0,0 +1,6 @@
+lista=['futbol','tenis','basquetball','champions']
+def palabras(lista):
+	lista.sort()
+	listafinal=(lista[0]+" , "+lista[1]+" , "+lista[2]+" , "+lista[3])
+	print listafinal
+palabras(lista)
