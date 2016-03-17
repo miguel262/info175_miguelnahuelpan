7def97345894b7b9c664e2819dd356f57c02a865
miguel262 <miguel.nahuelpan@alumnos.uach.cl>
2016-03-17 14:13:11 -0300

    Delete Ejercicio2.py

--- Ejercicio2.py
+++
@@ -1,6 +0,0 @@
-lista=['futbol','tenis','basquetball','champions']
-def palabras(lista):
-	lista.sort()
-	listafinal=(lista[0]+" , "+lista[1]+" , "+lista[2]+" , "+lista[3])
-	print listafinal
-palabras(lista)
